05e65fcab11a4c169099b460fef415fa6f6b9405
miguel-antoons <miguel.antoons@gmail.com>
2022-02-22 14:18:58 +0100

    add class namespaces, otherwise the code won't work

--- com_bramsdata/site/bramsdata.php
+++ com_bramsdata/site/bramsdata.php
@@ -9,6 +9,12 @@
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
 
+use \Joomla\CMS\MVC\Controller\BaseController;
+use \Joomla\CMS\MVC\View\HtmlView;
+use \Joomla\CMS\MVC\Model\BaseDatabaseModel;
+use \Joomla\CMS\MVC\Model\ItemModel;
+use \Joomla\CMS\MVC\Model\ListModel;
+
 // Get an instance of the controller prefixed by BramsData
 $controller = BaseController::getInstance('BramsData');
 
