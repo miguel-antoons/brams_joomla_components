a5945ff6892e70e6f8d110c423b1cbb162adf9c1
miguel-antoons <miguel.antoons@gmail.com>
2022-02-25 11:53:52 +0100

    debugging previously added optimisations

--- com_bramsdata/site/models/availability.php
+++ com_bramsdata/site/models/availability.php
@@ -113,6 +113,7 @@ class BramsDataModelAvailability extends ItemModel {
 			$specific_station_availability = array_filter(
 				$db_availability,
 				function($availability_info) {
+					echo $availability_info;
 					return $availability_info->system_id === $station;
 				}
 			);
