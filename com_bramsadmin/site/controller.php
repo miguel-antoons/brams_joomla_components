498442345efa47b99c91d124bf8b4dc964ffc4d3
miguel-antoons <miguel.antoons@gmail.com>
2022-03-18 15:36:10 +0100

    adding first version of system update

--- com_bramsadmin/site/controller.php
+++ com_bramsadmin/site/controller.php
@@ -17,20 +17,29 @@ use \Joomla\CMS\MVC\Controller\BaseController;
  * @since  0.0.1
  */
 class BramsAdminController extends BaseController {
-    public function display() {
-        $document = \JFactory::getDocument();
+    /**
+	 * Typical view method for MVC based architecture
+	 *
+	 * This function is provide as a default implementation, in most cases
+	 * you will need to override it in your own controllers.
+	 *
+	 * @param   boolean  $cachable   If true, the view output will be cached
+	 * @param   array    $urlparams  An array of safe URL parameters and their variable types, for valid values see {@link \JFilterInput::clean()}.
+	 *
+	 * @return  \JControllerLegacy  A \JControllerLegacy object to support chaining.
+	 *
+	 * @since   3.0
+	 */
+	public function display($block_display = false, $cachable = false, $urlparams = array())
+	{
+		$document = \JFactory::getDocument();
 		$viewType = $document->getType();
 		$viewName = $this->input->get('view', $this->default_view);
 		$viewLayout = $this->input->get('layout', 'default', 'string');
 
-		$view = $this->getView(
-            $viewName,
-            $viewType,
-            '',
-            array('base_path' => $this->basePath, 'layout' => $viewLayout)
-        );
+		$view = $this->getView($viewName, $viewType, '', array('base_path' => $this->basePath, 'layout' => $viewLayout));
 
-        // Get/Create the model
+		// Get/Create the model
 		if ($model = $this->getModel($viewName))
 		{
 			// Push the model into the view (as default)
@@ -39,16 +48,62 @@ class BramsAdminController extends BaseController {
 
 		$view->document = $document;
 
-        return $view;
-    }
+		// Display the view
+		if ($cachable && $viewType !== 'feed' && \JFactory::getConfig()->get('caching') >= 1)
+		{
+			$option = $this->input->get('option');
+
+			if (is_array($urlparams))
+			{
+				$app = \JFactory::getApplication();
+
+				if (!empty($app->registeredurlparams))
+				{
+					$registeredurlparams = $app->registeredurlparams;
+				}
+				else
+				{
+					$registeredurlparams = new \stdClass;
+				}
+
+				foreach ($urlparams as $key => $value)
+				{
+					// Add your safe URL parameters with variable type as value {@see \JFilterInput::clean()}.
+					$registeredurlparams->$key = $value;
+				}
+
+				$app->registeredurlparams = $registeredurlparams;
+			}
+
+			try
+			{
+				/** @var \JCacheControllerView $cache */
+				$cache = \JFactory::getCache($option, 'view');
+				$cache->get($view, 'display');
+			}
+			catch (\JCacheException $exception)
+			{
+				$view->display();
+			}
+		}
+		elseif(block_display)
+		{
+			return $view;
+		}
+        else {
+            $view->display();
+        }
+
+		return $this;
+	}
     
     public function newSystem() {
-        $view = $this->display();
+        $view = $this->display(true);
         $view->create();
     }
 
     public function updateSystem() {
-        $view = $this->display();
+        $view = $this->display(true);
         $view->update();
     }
 }
