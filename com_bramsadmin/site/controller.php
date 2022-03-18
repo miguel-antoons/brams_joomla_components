e646e1c899083ba84784ac43d2e62f9085e91100
miguel-antoons <miguel.antoons@gmail.com>
2022-03-18 15:31:57 +0100

    adding first version of system update

--- com_bramsadmin/site/controller.php
+++ com_bramsadmin/site/controller.php
@@ -17,7 +17,7 @@ use \Joomla\CMS\MVC\Controller\BaseController;
  * @since  0.0.1
  */
 class BramsAdminController extends BaseController {
-    public function newSystem() {
+    public function display() {
         $document = \JFactory::getDocument();
 		$viewType = $document->getType();
 		$viewName = $this->input->get('view', $this->default_view);
@@ -29,21 +29,26 @@ class BramsAdminController extends BaseController {
             '',
             array('base_path' => $this->basePath, 'layout' => $viewLayout)
         );
+
+        // Get/Create the model
+		if ($model = $this->getModel($viewName))
+		{
+			// Push the model into the view (as default)
+			$view->setModel($model, true);
+		}
+
+		$view->document = $document;
+
+        return $view;
+    }
+    
+    public function newSystem() {
+        $view = $this->display();
         $view->create();
     }
 
     public function updateSystem() {
-        $document = \JFactory::getDocument();
-		$viewType = $document->getType();
-		$viewName = $this->input->get('view', $this->default_view);
-		$viewLayout = $this->input->get('layout', 'default', 'string');
-
-		$view = $this->getView(
-            $viewName,
-            $viewType,
-            '',
-            array('base_path' => $this->basePath, 'layout' => $viewLayout)
-        );
+        $view = $this->display();
         $view->update();
     }
 }
